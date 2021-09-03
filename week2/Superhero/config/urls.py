04791c3ba8e9217777fc391fd47058fb11aeba88
Cody <cox9176@bears.unco.edu>
2021-09-03 13:46:59 -0600

    Replace all hero templates with single template and add profile template

--- week2/Superhero/config/urls.py
+++ week2/Superhero/config/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hero.views import SupermanView, BatmanView, WonderWomanView
+from hero.views import SupermanView, BatmanView, WonderWomanView, HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view()),
     path('superman', SupermanView.as_view()),
     path('batman', BatmanView.as_view()),
     path('wonderwoman', WonderWomanView.as_view()),
--- week2/Superhero/hero/views.py
+++ week2/Superhero/hero/views.py
@@ -1,16 +1,28 @@
 from django.views.generic import TemplateView
 
 class SupermanView(TemplateView):
-    template_name = 'superman.html'
+    template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
         return {
-            'title': 'About this Class', 
-            'body': 'Once upon a time ...',
+            'hero': 'superman', 
         }
 
 class BatmanView(TemplateView):
-    template_name = "batman.html"
+    template_name = "hero.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'hero': 'batman', 
+        }
 
 class WonderWomanView(TemplateView):
-    template_name = "wonder_woman.html"
+    template_name = "hero.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'hero': 'wonder_woman', 
+        }
+
+class HomeView(TemplateView):
+    template_name = "profile.html"
