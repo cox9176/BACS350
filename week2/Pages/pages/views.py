958f567b1a6a052497a5b55f21e59c6289fa593c
Cody <cox9176@bears.unco.edu>
2021-09-03 12:44:50 -0600

    Change templates to paramaterized

---
+++ week2/Pages/pages/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import TemplateView
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'About this Class', 
+            'body': 'Once upon a time ...',
+        }
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My Home Page', 
+            'body': 'This page is boring ...',
+        }
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Index Page', 
+            'body': 'temp',
+        }
